6666aae8de1436cf9c65974877c49be142be72b7
CristianVelazquez1995 <cristian1995@gmail.com>
2021-04-30 10:18:33 -0300

    billing and client

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = Express();
 
+app.use(Express.json());
 app.use(
   cookiesSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +24,16 @@ app.use(Passport.initialize());
 app.use(Passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(Express.static('client/build'));
+
+  const path = require(path);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -21,16 +21,15 @@ Passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToke, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
